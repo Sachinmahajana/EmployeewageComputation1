bddc134722b8426d48bb14d423816d4cfd6f9325
Sachinmahajana <sachinmahajan782@gmail.com>
2023-01-20 09:43:48 +0530

    [Sachin] Adding Employeewagecomputation1

---
+++ EmployeeWageComputation1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageComputation1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Emplployeewage computation program");
+        }
+    }
+}
