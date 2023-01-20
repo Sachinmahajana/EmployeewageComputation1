70e4f80002b114deda870665533384c031ccac2a
Sachinmahajana <sachinmahajan782@gmail.com>
2023-01-20 14:52:00 +0530

    [Sachin] Modified Calculating EmployeeAttendence

--- EmployeeWageComputation1/Program.cs
+++ EmployeeWageComputation1/Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Emplployeewage computation program");
+            // UC 1 EmployeeAttendence
+            int isPresent = 1;
+            Random myObj = new Random();
+            int empAttendence = myObj.Next(0, 2);
+
+            if (empAttendence == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+            Console.ReadLine();
         }
     }
 }
