5938075c62436145f9a1623bc5dfd4a31af49ec2
Sachinmahajana <sachinmahajan782@gmail.com>
2023-01-22 00:11:53 +0530

    [Sachin] Adding Switccase in Employeewagecomputation

--- EmployeeWageComputation1/Program.cs
+++ EmployeeWageComputation1/Program.cs
@@ -5,27 +5,28 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Emplployeewage computation program");
-            // UC 3 EmployeeWageAddparttime
+            // UC4 EmpoyeewageAddPartTime by using Switchcase
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             int empHrs = 0, empWage = 0;
             Random myObj = new Random();
             int empAttendence = myObj.Next(0,3);
-            if (empAttendence == FULL_TIME)
+
+            switch (empAttendence)
             {
-                Console.WriteLine("Employee is Present");
-                empHrs = 8;
-            }
-            else if (empAttendence == PART_TIME)
-            {
-                Console.WriteLine("Employee is Present");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Employee is Absent");
-                empHrs = 0;
+                case FULL_TIME:
+                    Console.WriteLine("Employee is Present");
+                    empHrs = 8;
+                    break;
+                case PART_TIME:
+                    Console.WriteLine("Employee is Present");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HR;
             Console.WriteLine("Employeewage {0} wage", empWage);
