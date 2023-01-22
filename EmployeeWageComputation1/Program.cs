9fdc234a6699cb412336e7663fd88c0c89ef5c03
Sachinmahajana <sachinmahajan782@gmail.com>
2023-01-22 14:11:43 +0530

    [Sachin] Adding Calculatewage for month

--- EmployeeWageComputation1/Program.cs
+++ EmployeeWageComputation1/Program.cs
@@ -5,37 +5,41 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Emplployeewage computation program");
-            // UC4 EmpoyeewageAddPartTime by using Switchcase
+            // UC 5 CalculateWageForMonth
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
+            const int MAX_WORKING_DAYS = 20;
             const int EMP_RATE_PER_HR = 20;
-            int empHrs = 0, empWage = 0;
+            int empHrs = 0, empWage = 0, day = 1, totalWage = 0;
             Random myObj = new Random();
-            int empAttendence = myObj.Next(0,3);
-
-            switch (empAttendence)
+            for (day = 1; day <= MAX_WORKING_DAYS; day++)
             {
-                case FULL_TIME:
-                    Console.WriteLine("Employee is Present");
-                    empHrs = 8;
-                    break;
-                case PART_TIME:
-                    Console.WriteLine("Employee is Present");
-                    empHrs = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    empHrs = 0;
-                    break;
+                int empAttendence = myObj.Next(0, 3);
+                switch (empAttendence)
+                {
+                    case FULL_TIME:
+                        Console.WriteLine("Employee is Present");
+                        empHrs = 8;
+                        break;
+                    case PART_TIME:
+                        Console.WriteLine("Employee is Present");
+                        empHrs = 4;
+                        break;
+                    default:
+                        Console.WriteLine("Employee is Absent");
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HR;
+                Console.WriteLine("Day {0} Employeewage: {1}", day, empWage);
+                totalWage += empWage;
             }
-            empWage = empHrs * EMP_RATE_PER_HR;
-            Console.WriteLine("Employeewage {0} wage", empWage);
+            Console.WriteLine("Total Employeewage for {0} days: {1}", (day - 1), totalWage);
             Console.ReadLine();
         }
-     }
+    }
+
 }
-
-
 
 
 
